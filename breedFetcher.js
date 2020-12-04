11625c7c6e4a65ceef602ff4e80d4576501a26e6
Samicap <lovestochasewindmills@gmail.com>
2020-12-04 17:41:34 +0000

    removed some console.logs

--- breedFetcher.js
+++ breedFetcher.js
@@ -11,8 +11,8 @@ request(searchURL, (error, response, body) => {
   } else { 
     //   //need to get the error code to show 404 when breed not found?
     // }
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     // console.log(typeof body)
     //console.log('body:', body); // Print the HTML for the Google homepage.
     
